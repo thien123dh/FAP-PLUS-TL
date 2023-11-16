4457c7edca313dbe214259f1aaa25ba38756f966
thien123dh <134196273+thien123dh@users.noreply.github.com>
2023-11-16 21:52:42 +0700

    first commit

---
+++ src/main/java/com/example/fap_plus/FapPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.fap_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FapPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fap_plus/FapPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fap_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FapPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
