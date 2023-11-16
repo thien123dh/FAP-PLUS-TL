15b62c2e5ade0711a661d998961c355eb267a2be
thien123dh <134196273+thien123dh@users.noreply.github.com>
2023-11-16 23:23:19 +0700

    create entity/Student

---
+++ src/main/java/com/example/fap_plus/entity/Student.java
@@ -0,0 +1,22 @@
+package com.example.fap_plus.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer yearOfBirth;
+
+}
